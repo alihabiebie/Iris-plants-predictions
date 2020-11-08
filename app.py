609a492df99da4649986f796daaebeb75aa6ba70
alihabiebie <62591088+alihabiebie@users.noreply.github.com>
2020-11-08 22:18:16 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify, request
+import pickle
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/iris')
+def iris():
+    return render_template('iris.html')
+
+@app.route('/hasil', methods=['POST'])
+def klasifikasi():
+    if request.method == 'POST':
+        input = request.form
+        sl = float(input['sl'])
+        sw = float(input['sw'])
+        pl = float(input['pl'])
+        pw = float(input['pw'])
+        pred = Iris.predict([[sl, sw, pl, pw]])[0]
+
+        return render_template('result.html', data=input, prediksi=pred)
+
+if __name__ == "__main__":
+    ### Load Model
+    with open('modelIris', 'rb') as ML:
+        Iris = pickle.load(ML)
+    app.run(debug=True)
